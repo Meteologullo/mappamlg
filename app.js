91bc72441dc27d459fbff5f7f4aea75ee2b0a085
Meteologullo <angelologullo21@gmail.com>
2025-05-22 16:24:51 +0200

    Delete app.js

--- app.js
+++
@@ -1,9 +0,0 @@
-export function aggiornaMappa(tipo) {
-  console.log("Visualizza mappa:", tipo);
-  // Qui andrà la logica di aggiornamento dei marker in base al tipo selezionato
-}
-
-// Caricamento iniziale
-window.addEventListener('DOMContentLoaded', () => {
-  aggiornaMappa('attuali');
-});
